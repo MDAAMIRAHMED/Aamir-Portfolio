ca505e868588088e89e9e0c3191bcbb55aba9109
Mohammed Aamir Ahmed <118830391+MDAAMIRAHMED@users.noreply.github.com>
2024-09-07 15:52:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-var typed = new Typed('#element', {
-    strings: ['Web Developer', 'Python Developer', 'Java Developer', 'AI/ML Engineer'],
-    typeSpeed: 50,
-    backSpeed: 30,
-    backDelay: 1000,
-    loop: true
+var typed = new Typed("#element", {
+  strings: [
+    "Web Developer",
+    "Python Developer",
+    "Java Developer",
+    "AI/ML Engineer",
+  ],
+  typeSpeed: 50,
+  backSpeed: 30,
+  backDelay: 1000,
+  loop: true,
 });
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
+
+function toggleMenu() {
+  const navLinks = document.getElementById("nav-links");
+  if (navLinks.style.display === "block") {
+      navLinks.style.display = "none";
+  } else {
+      navLinks.style.display = "block";
+  }
+}
